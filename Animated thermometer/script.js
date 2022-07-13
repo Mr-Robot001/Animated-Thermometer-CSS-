ed5d2ae568c62b99ab642137ffef719434e79af1
Ankit <88779428+Mr-Robot001@users.noreply.github.com>
2022-07-13 23:41:20 +0530

    Add files via upload

---
+++ Animated thermometer/script.js
@@ -0,0 +1,30 @@
+
+const tempLoad = () => {
+
+    let temp = document.getElementById('temp');
+    temp.innerHTML =  "&#xf2cb";
+    temp.style.color = "yellow";
+   
+    
+    setTimeout(()=>{
+        temp.innerHTML = "&#xf2ca";
+        
+    }, 1000);
+
+    setTimeout(()=>{
+        temp.innerHTML = "&#xf2c9";
+    }, 2000);
+
+    setTimeout(()=>{
+        temp.innerHTML = "&#xf2c8";
+    }, 3000);
+
+    setTimeout(()=>{
+        temp.innerHTML = "&#xf2c7";
+        temp.style.color = "red";
+    }, 4000);
+   
+}
+tempLoad();
+setInterval(tempLoad , 5000) ;
+    
